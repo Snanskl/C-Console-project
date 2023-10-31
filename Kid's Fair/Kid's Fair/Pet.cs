052be3c15bd6af2d9b24664b2a7694f534f52ae4
Minou <118366099+Snanskl@users.noreply.github.com>
2023-10-31 18:11:48 +0100

    Add files via upload

---
+++ Kid's Fair/Kid's Fair/Pet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kid_s_Fair
+{
+    internal class Pet
+    {
+        //Declaring needed fields that will be used within the class
+        private string petName;
+        private int petAge;
+        private bool petIsFemale;
+
+        //A method that "collect" all the other method to be invoke in Main method
+        public void Start()
+        {
+            Readinginput_Pet();
+            FinalOutput_Pet();
+        }
+
+        //A method for reading user's input on fields 
+        private void Readinginput_Pet()
+        {
+            Console.WriteLine("Greeting from MyPet class\n"); //using excape sequence "new line" to create space
+            
+
+            Console.Write("What is the name of your pet? ");
+            petName = Console.ReadLine(); // The answer will be stored in "petName" variable
+
+            // I like using string literal "$" for make the code more readable.
+            Console.Write($"What is {petName}'s age? ");
+            //Using int.Parse conversion because the input value is a string datatype.
+            petAge = int.Parse(Console.ReadLine()); 
+
+            Console.Write($"Is {petName} a female (y/n)? ");
+            string answer = Console.ReadLine().ToLower(); // Using "ToLower" method to to control user's input.
+
+            if (answer == "y") //if the input is "Y" or "y", bollean will turn to true.
+            {petIsFemale = true;}
+
+            else
+            { petIsFemale = false; }
+            Console.WriteLine();
+        }
+        //A method to display value
+        private void FinalOutput_Pet()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+            Console.WriteLine($"Name: {petName} Age: {petAge}");
+            Console.WriteLine($"{petName} is a good girl!");
+            Console.WriteLine("+++++++++++++++++++++++++++++\n"); // using escape sequence "new line" to create space
+            Console.WriteLine("Pess Enter to start the next part!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kid's Fair/Kid's Fair/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kid_s_Fair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet pet1 = new Pet(); //Instantiated an objetc from Pet class
+            pet1.Start(); //Invoke methods that we've collected in "Start"
+
+            Album album1 = new Album(); //Instantiated an object from Album class
+            album1.Start(); // Invoke methods that we've collected in "Start"
+            
+            TicketSeller ticket = new TicketSeller(); //Instantiated an object from TicketSeller class
+            ticket.Start(); //Invoke methods that we've collected in "Start"
+        }
+    }
+}
